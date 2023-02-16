32fd49174280d9927576fef35cbd74c5c1bc37d7
musaibahmedrazzaqui <104699056+musaibahmedrazzaqui@users.noreply.github.com>
2023-02-17 02:22:18 +0500

    Update App.js

--- App.js
+++ App.js
@@ -31,6 +31,10 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server started on port 3000");
+// server.listen(3000, () => {
+//   console.log("Server started on port 3000");
+// });
+var port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server running on port: ` + port);
 });
