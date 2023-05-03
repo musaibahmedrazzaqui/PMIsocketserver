e455f5b9a4d9b05b15cd22d58551bc712b8ec1b8
Musaib Ahmed Razzaqui <musaibahmed2000@gmail.com>
2023-05-03 22:52:17 +0500

    Update App.js

--- App.js
+++ App.js
@@ -6,38 +6,46 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 const interval = 5000;
-// Driver's location data
-let driverLocation = null;
+const driverLocations = new Map();
 
 // Driver sends location data to server via HTTP POST request
 app.post("/driver-location", express.json(), (req, res) => {
-  const location = req.body;
-  driverLocation = location;
-  console.log("Driver location:", driverLocation);
+  const { driverId, location } = req.body;
+  driverLocations.set(driverId, location);
+  console.log(`Driver ${driverId} location:`, location);
+
+  // Send driver location updates to all users subscribed to the driver's room
+  io.to(driverId).emit("driverLocation", location);
+
   res.sendStatus(200);
 });
 
-// User requests driver's location data via Socket.IO
+// User subscribes to a driver's location updates via Socket.IO
 io.on("connection", (socket) => {
   console.log("User connected");
 
-  // Send driver's location to user
-  if (driverLocation) {
-    setInterval(() => {
-      socket.emit("driverLocation", driverLocation);
-      console.log("Location data sent");
-    }, interval);
-  }
+  socket.on("subscribe", (driverId) => {
+    socket.join(driverId);
+    console.log(`User subscribed to driver ${driverId}`);
+
+    // Send driver's location to user
+    if (driverLocations.has(driverId)) {
+      socket.emit("driverLocation", driverLocations.get(driverId));
+      console.log(`Location data for driver ${driverId} sent`);
+    }
+  });
+
+  socket.on("unsubscribe", (driverId) => {
+    socket.leave(driverId);
+    console.log(`User unsubscribed from driver ${driverId}`);
+  });
 
   socket.on("disconnect", () => {
     console.log("User disconnected");
   });
 });
 
-// server.listen(3000, () => {
-//   console.log("Server started on port 3000");
-// });
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
-  console.log(`Server running on port: ` + port);
+  console.log(`Server running on port: ${port}`);
 });
